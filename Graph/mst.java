d2393efcc64b679e3d3601ea39e218aca80fa3e9
paulvinueza30 <paulvinueza30@gmail.com>
2025-04-28 22:15:38 -0400

    add all basic graph algos

---
+++ Graph/mst.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class mst {
+    static final int V = 5;
+
+    int minKey(int key[], Boolean mstSet[]) {
+        int min = Integer.MAX_VALUE, min_index = -1;
+        for (int v = 0; v < V; v++)
+            if (mstSet[v] == false && key[v] < min) {
+                min = key[v];
+                min_index = v;
+            }
+        return min_index;
+    }
+
+    void primMST(int graph[][]) {
+        int parent[] = new int[V];
+        int key[] = new int[V];
+        Boolean mstSet[] = new Boolean[V];
+        Arrays.fill(key, Integer.MAX_VALUE);
+        Arrays.fill(mstSet, false);
+        key[0] = 0;
+        parent[0] = -1;
+        for (int count = 0; count < V - 1; count++) {
+            int u = minKey(key, mstSet);
+            mstSet[u] = true;
+            for (int v = 0; v < V; v++)
+                if (graph[u][v] != 0 && mstSet[v] == false && graph[u][v] < key[v]) {
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+        }
+    }
+
+    public static void main(String[] args) {
+        int graph[][] = new int[][] { { 0, 2, 0, 6, 0 },
+                { 2, 0, 3, 8, 5 },
+                { 0, 3, 0, 0, 7 },
+                { 6, 8, 0, 0, 9 },
+                { 0, 5, 7, 9, 0 } };
+        mst t = new mst();
+        t.primMST(graph);
+    }
+}
